9b124402c630979e2893f0abf5cc323d6697a692
Peter Thompson <62572056+pthompson-cpi@users.noreply.github.com>
2021-08-12 14:53:59 -0700

    lua fixes and python tools
    * cleans up lua code, break out functions and structure for easier EXI decode
    * adds python util to auto-decrypt TLS traffic if a session key can be found
    
    Signed-off-by: jhart-cpi <jeffrey.hart@chargepoint.com>

--- README.md
+++ README.md
@@ -15,7 +15,51 @@ the output.
     -X lua_script:v2g.lua -r test.pcap
 ```
 
+### Win 10
+
+Install wireshark application in the usual location, and this will
+allow access to the tshark application to run the script and debug
+the output.
+
+```
+c:\Program Files (x86)\Wireshark\tshark.exe-X lua_script:v2g.lua -r test.pcap
+```
+
 ## Decrypting TLS payloads
 
-This LUA code will automatically decrypt the V2G traffic if a UDP packet is
-emitted on port 15118 with the TLS key used to ecrypt the session.
+### Automatic method
+
+Under the tools folder is a simple python script that will take a V2G
+debug packet capture, extract the secret, use wireshark's `editcap`
+utility to extract the TLS session key and apply it across the capture.
+
+#### Prereq installation
+If you have python3 installed, you can install `scapy` or use `pipenv` which
+is included in the `tools/` directory.
+
+To install scapy without pipenv: `python3 -m pip install scapy==2.4.5`
+
+With pipenv: `pipenv shell`
+
+#### Running `extract_secrets.py`
+
+```shell
+$ ./extract_secrets.py -f ../pcaps/plc_20210810T190140.pcap
+WARNING: No IPv4 address found on en3 !
+WARNING: No IPv4 address found on en4 !
+WARNING: more No IPv4 address found on en1 !
+TLS was requested by fe80::218:23ff:fe0f:fbfe:49156
+found potential session key on raw UDP packet with dest port 49156
+writing ../pcaps/plc_20210810T190140_secret.txt
+editcap --inject-secrets tls,../pcaps/plc_20210810T190140_secret.txt ../pcaps/plc_20210810T190140.pcap ../pcaps/plc_20210810T190140_decrypted.pcap
+```
+
+### Manual method
+
+Most debugging captures will record the TLS session key with a single UDP
+packet sent to the Vehicle port from a random port. Inside you'll find a key
+like the following:
+
+```
+CLIENT_RANDOM 6112d032475e758bbf8eaf2b0a540f3f2c7a6f6d1a9b48935d16c468086822f5 8471a233b4f1926084b68977a28ef8b65f59fea4b4942800539ba1d991a98c3f81e29a109d394606bd91286981dbd122
+```
---
+++ tools/extract_secrets.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+from scapy.layers.inet6 import UDP as UDP6
+from scapy.layers.inet6 import IPv6
+from pathlib import Path
+import argparse
+import subprocess
+
+
+sdp_request_type = {
+    "sdp_req": 0x9000,
+    "sdp_resp": 0x9001,
+    "exi": 0x8001,
+}
+
+SECURITY_FIELD = XByteEnumField("security", 0, {0x00: "True", 0x10: "False"})
+TRANSPORT_FIELD = XByteEnumField("transport", 0, {0x00: "TCP", 0x10: "UDP"})
+
+
+class SDP(Packet):
+    name = "SECC Discovery Protocol"
+    fields_desc = [
+        XByteField("version", 1),
+        XByteField("inverted_version", 1),
+        XShortEnumField("payload_type", sdp_request_type["sdp_req"],
+                        {v: k for k, v in sdp_request_type.items()}),
+        IntField("payload_size", 0),
+    ]
+
+
+class SDPRequest(Packet):
+    name = "SECC Discovery Protocol Request"
+    fields_desc = [
+        SECURITY_FIELD,
+        TRANSPORT_FIELD,
+    ]
+
+
+class SDPResponse(Packet):
+    name = "SECC Discovery Protocol Response"
+    fields_desc = [
+        IP6Field("secc_ip_addr", "::1"),
+        ShortField("secc_port", 0),
+        SECURITY_FIELD,
+        TRANSPORT_FIELD,
+    ]
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--file", help="path to pcap file", required=True)
+    ap.add_argument("-v", "--verbose", help="print packets", action="count", default=0)
+
+    bind_layers(IPv6, UDP6)
+    bind_layers(UDP6, SDP, dport=15118)
+    bind_layers(UDP6, SDP, sport=15118)
+    bind_layers(SDP, SDPRequest, payload_type=sdp_request_type["sdp_req"])
+    bind_layers(SDP, SDPResponse, payload_type=sdp_request_type["sdp_resp"])
+
+    args = ap.parse_args()
+    cap = rdpcap(args.file)
+    secure_packet_port = None
+    has_security = False
+    tls_key = None
+
+    for pkt in cap:
+        if args.verbose > 0 and pkt.haslayer(SDP):
+            pkt.show()
+
+        if pkt.haslayer(SDPRequest):
+            if not bool(pkt.security):
+                secure_packet_port = pkt[UDP6].sport
+                print(f"TLS was requested by {pkt[IPv6].src}:{secure_packet_port}")
+                has_security = True
+            else:
+                print("Session did not request security, no secret to extract")
+
+        if has_security and not pkt.haslayer(SDP):
+            if pkt.haslayer(UDP6) and pkt[UDP6].dport == secure_packet_port:
+                print(f"found potential session key on raw UDP packet with dest port {secure_packet_port}")
+                tls_key = pkt[UDP6].load.decode("utf8", "ignore")
+
+    if tls_key is not None:
+        old_file = Path(args.file)
+        new_file = Path(f'{old_file.parent}/{old_file.name.replace(old_file.suffix, "_secret.txt")}')
+        decrypted_out = Path(f'{old_file.parent}/{old_file.name.replace(old_file.suffix, "_decrypted.pcap")}')
+        print(f"writing {new_file}")
+        with Path(new_file).open(mode="w") as f:
+            f.write(tls_key)
+
+        cmd = f"editcap --inject-secrets tls,{new_file} {old_file} {decrypted_out}"
+
+        try:
+            print(cmd)
+            subprocess.run(cmd.split(), check=True)
+            return
+        except subprocess.CalledProcessError:
+            print("cannot find 'editcap' command")
+
+    print("unable to find TLS session key")
+
+
+if __name__ == '__main__':
+    main()
