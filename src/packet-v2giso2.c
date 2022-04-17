aed3747fd917d58d116ee85d83411ecb262c013f
Charles Hardin <charles.hardin@chargepoint.com>
2022-04-16 22:44:51 -0700

    dissector: restructure the dissectors for calling iso1 and iso2
    Extended the dissector support to additional registered iso1 and
    iso2 packet code. This is an interim commit to move some of
    common utilities to v2gexi and test some display issues on
    wireshark vs tshark for display issues.
    
    The tshark will just call the dissectors with a tree pointer
    that has been allocated, but wireshark does a two pass call
    with a tree nil pointer and then a second pass with a valid
    tree pointer - so, add the handshake for decode parts into
    that.
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

--- src/packet-v2giso2.c
+++ src/packet-v2giso2.c
@@ -16,33 +16,33 @@
 
 /* openv2g */
 #include <codec/EXITypes.h>
-#include <iso1/iso1EXIDatatypes.h>
-#include <iso1/iso1EXIDatatypesDecoder.h>
+#include <iso2/iso2EXIDatatypes.h>
+#include <iso2/iso2EXIDatatypesDecoder.h>
 
 #include "v2gexi.h"
 
 
 /* forward declare */
-void proto_register_v2giso1(void);
-void proto_reg_handoff_v2giso1(void);
+void proto_register_v2giso2(void);
+void proto_reg_handoff_v2giso2(void);
 
 
 static dissector_handle_t v2gexi_handle;
 
-static int proto_v2giso1 = -1;
+static int proto_v2giso2 = -1;
 
-static int hf_v2giso1_struct_iso1MessageHeaderType_SessionID = -1;
+static int hf_v2giso2_struct_iso2MessageHeaderType_SessionID = -1;
 
 /* Initialize the subtree pointers */
-static gint ett_v2giso1 = -1;
-static gint ett_v2giso1_header = -1;
-static gint ett_v2giso1_body = -1;
-static gint ett_v2giso1_array = -1;
-static gint ett_v2giso1_array_i = -1;
+static gint ett_v2giso2 = -1;
+static gint ett_v2giso2_header = -1;
+static gint ett_v2giso2_body = -1;
+static gint ett_v2giso2_array = -1;
+static gint ett_v2giso2_array_i = -1;
 
 
 static void
-dissect_v2giso1_header(const struct iso1MessageHeaderType *header,
+dissect_v2giso2_header(const struct iso2MessageHeaderType *header,
 		       tvbuff_t *tvb,
 		       proto_tree *tree,
 		       gint idx,
@@ -54,16 +54,11 @@ dissect_v2giso1_header(const struct iso1MessageHeaderType *header,
 		tvb, 0, 0, idx, NULL, subtree_name);
 
 	exi_add_bytes(subtree,
-		hf_v2giso1_struct_iso1MessageHeaderType_SessionID,
+		hf_v2giso2_struct_iso2MessageHeaderType_SessionID,
 		tvb,
 		header->SessionID.bytes,
 		header->SessionID.bytesLen,
 		sizeof(header->SessionID.bytes));
-
-	if (header->Notification_isUsed) {
-		/* Notification */
-		proto_tree_add_debug_text(subtree, "TODO - Notification");
-	}
 
 	if (header->Signature_isUsed) {
 		/* Notification */
@@ -74,7 +69,7 @@ dissect_v2giso1_header(const struct iso1MessageHeaderType *header,
 }
 
 static void
-dissect_v2giso1_body(const struct iso1BodyType *body,
+dissect_v2giso2_body(const struct iso2BodyType *body,
 		     tvbuff_t *tvb,
 		     proto_tree *tree,
 		     gint idx,
@@ -90,15 +85,15 @@ dissect_v2giso1_body(const struct iso1BodyType *body,
 }
 
 static int
-dissect_v2giso1(tvbuff_t *tvb,
+dissect_v2giso2(tvbuff_t *tvb,
 		packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
 	size_t pos;
 	bitstream_t stream;
 	int errn;
-	struct iso1EXIDocument exiiso1;
+	struct iso2EXIDocument exiiso2;
 
-	col_set_str(pinfo->cinfo, COL_PROTOCOL, "ISO1");
+	col_set_str(pinfo->cinfo, COL_PROTOCOL, "ISO2");
 	/* Clear the info column */
 	col_clear(pinfo->cinfo, COL_INFO);
 
@@ -107,69 +102,69 @@ dissect_v2giso1(tvbuff_t *tvb,
 	stream.pos = &pos;
 	stream.data = tvb_memdup(wmem_packet_scope(),
 				 tvb, pos, stream.size);
-	errn = decode_iso1ExiDocument(&stream, &exiiso1);
+	errn = decode_iso2ExiDocument(&stream, &exiiso2);
 	if (errn != 0) {
 		/* decode failed */
 		return 0;
 	}
 
 	/*
-	 * Everything in ISO1 should come in as a messagge
+	 * Everything in ISO2 should come in as a messagge
 	 * - Header
 	 * - Body
 	 */
-	if (exiiso1.V2G_Message_isUsed) {
-		proto_tree *v2giso1_tree;
+	if (exiiso2.V2G_Message_isUsed) {
+		proto_tree *v2giso2_tree;
 
-		v2giso1_tree = proto_tree_add_subtree(tree,
-			tvb, 0, 0, ett_v2giso1, NULL, "V2G Message");
+		v2giso2_tree = proto_tree_add_subtree(tree,
+			tvb, 0, 0, ett_v2giso2, NULL, "V2G Message");
 
-		dissect_v2giso1_header(&exiiso1.V2G_Message.Header,
-			tvb, v2giso1_tree, ett_v2giso1_header, "Header");
-		dissect_v2giso1_body(& exiiso1.V2G_Message.Body,
-			tvb, v2giso1_tree, ett_v2giso1_body, "Body");
+		dissect_v2giso2_header(&exiiso2.V2G_Message.Header,
+			tvb, v2giso2_tree, ett_v2giso2_header, "Header");
+		dissect_v2giso2_body(& exiiso2.V2G_Message.Body,
+			tvb, v2giso2_tree, ett_v2giso2_body, "Body");
 	}
 
 	return tvb_captured_length(tvb);
 }
 
 void
-proto_register_v2giso1(void)
+proto_register_v2giso2(void)
 {
 
 	static hf_register_info hf[] = {
-		/* struct iso1MessageHeaderType */
-		{ &hf_v2giso1_struct_iso1MessageHeaderType_SessionID,
-		  { "SessionID", "v2giso1.struct.messageheader.sessionid",
+		/* struct iso2MessageHeaderType */
+		{ &hf_v2giso2_struct_iso2MessageHeaderType_SessionID,
+		  { "SessionID", "v2giso2.struct.messageheader.sessionid",
 		    FT_STRING, BASE_NONE, NULL, 0x0, NULL, HFILL }
 		}
 	};
 
 	static gint *ett[] = {
-		&ett_v2giso1,
-		&ett_v2giso1_header,
-		&ett_v2giso1_body,
-		&ett_v2giso1_array,
-		&ett_v2giso1_array_i,
+		&ett_v2giso2,
+		&ett_v2giso2_header,
+		&ett_v2giso2_body,
+		&ett_v2giso2_array,
+		&ett_v2giso2_array_i,
 	};
 
-	proto_v2giso1 = proto_register_protocol(
-		"V2G Efficient XML Interchange (ISO1)",
-		"V2GISO1",
-		"v2giso1"
+	proto_v2giso2 = proto_register_protocol(
+		"V2G Efficient XML Interchange (ISO2)",
+		"V2GISO2",
+		"v2giso2"
 	);
-	proto_register_field_array(proto_v2giso1, hf, array_length(hf));
+	proto_register_field_array(proto_v2giso2, hf, array_length(hf));
 	proto_register_subtree_array(ett, array_length(ett));
 
-	register_dissector("v2giso1", dissect_v2giso1, proto_v2giso1);
+	register_dissector("v2giso2", dissect_v2giso2, proto_v2giso2);
 }
 
 void
-proto_reg_handoff_v2giso1(void)
+proto_reg_handoff_v2giso2(void)
 {
 
 	/* add a handle for the connection oriented V2G EXI */
-	v2gexi_handle = find_dissector_add_dependency("v2gexi", proto_v2giso1);
+	v2gexi_handle = find_dissector_add_dependency("v2gexi", proto_v2giso2);
 }
 
 /*
