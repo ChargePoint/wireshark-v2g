d96319770de86fed31f3e00a43116cf9a9ee1d2e
Charles Hardin <charles.hardin@chargepoint.com>
2023-05-20 15:00:14 -0700

    tools/vagrant: update the box reference to Ubuntu 22.04
    Trying to reproduce the standalone issue reported for unknown symbols
    
        # vagrant adds the wireshark-dev repo and packages
        vagrant up
        vagrant ssh
    
        mkdir build && cd build
        cmake ../wireshark-v2g
        make
        sudo make install
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

---
+++ src/v2gplugin.c
@@ -0,0 +1,52 @@
+/*
+ * Do not modify this file. Changes will be overwritten.
+ *
+ * Generated automatically from make-plugin-reg.py.
+ */
+
+/* plugins are DLLs on Windows */
+#define WS_BUILD_DLL
+#include "ws_symbol_export.h"
+
+#include <wireshark.h>
+#include <epan/packet.h>
+#include <epan/proto.h>
+
+void proto_register_v2gdin(void);
+void proto_register_v2gexi(void);
+void proto_register_v2giso1(void);
+void proto_register_v2giso2(void);
+void proto_reg_handoff_v2gdin(void);
+void proto_reg_handoff_v2gexi(void);
+void proto_reg_handoff_v2giso1(void);
+void proto_reg_handoff_v2giso2(void);
+
+WS_DLL_PUBLIC_DEF const gchar plugin_version[] = VERSION;
+WS_DLL_PUBLIC_DEF const int plugin_want_major = WIRESHARK_VERSION_MAJOR;
+WS_DLL_PUBLIC_DEF const int plugin_want_minor = WIRESHARK_VERSION_MINOR;
+
+WS_DLL_PUBLIC void plugin_register(void);
+
+void plugin_register(void)
+{
+    static proto_plugin plug_v2gdin;
+
+    plug_v2gdin.register_protoinfo = proto_register_v2gdin;
+    plug_v2gdin.register_handoff = proto_reg_handoff_v2gdin;
+    proto_register_plugin(&plug_v2gdin);
+    static proto_plugin plug_v2gexi;
+
+    plug_v2gexi.register_protoinfo = proto_register_v2gexi;
+    plug_v2gexi.register_handoff = proto_reg_handoff_v2gexi;
+    proto_register_plugin(&plug_v2gexi);
+    static proto_plugin plug_v2giso1;
+
+    plug_v2giso1.register_protoinfo = proto_register_v2giso1;
+    plug_v2giso1.register_handoff = proto_reg_handoff_v2giso1;
+    proto_register_plugin(&plug_v2giso1);
+    static proto_plugin plug_v2giso2;
+
+    plug_v2giso2.register_protoinfo = proto_register_v2giso2;
+    plug_v2giso2.register_handoff = proto_reg_handoff_v2giso2;
+    proto_register_plugin(&plug_v2giso2);
+}
